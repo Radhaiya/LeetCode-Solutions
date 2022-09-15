70a04aeac6562ff9aab85771ea1179b80bb9f12a
Radhaiya <kulkarnir27999@gmail.com>
2022-09-15 22:36:21 +0530

    14 Days/DS 1

---
+++ Day 2/Two sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>mp;
+    
+    for(int i=0;i<nums.size();i++)
+    {
+        mp[nums[i]]=i;
+    }
+    for(int i=0;i<nums.size();i++)
+    {
+        if(mp[target-nums[i]])
+        {
+            if(mp[target-nums[i]]!=i)
+            {
+            return {i,mp[target-nums[i]]};
+            }
+        }
+    }
+    return {-1,-1};
+    }
+};
