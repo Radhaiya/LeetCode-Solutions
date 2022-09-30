ad9edae932f5c2c772fa3754101cc9af5f333229
Radhaiya <kulkarnir27999@gmail.com>
2022-09-30 11:29:32 +0530

    Run 3/ submit 1

---
+++ Arrays/1720. Decode XORed Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        
+        vector<int>ans; 
+        ans.push_back(first);
+        for(int i=1;i<encoded.size()+1;i++){
+           
+           ans.push_back(ans[i-1]^encoded[i-1]);
+        }
+
+        return ans;
+    }
+};
