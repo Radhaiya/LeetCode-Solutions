5dc6fa0bd99e799a3d38a718cf8796f4eb5c28b4
Radhaiya <kulkarnir27999@gmail.com>
2022-10-02 13:37:07 +0530

    Run 1 / Submit 1

---
+++ Arrays/1588. Sum of All Odd Length Subarrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int occur=0;
+        int n=arr.size();
+        int prod=1;
+        int sum=0;
+        for(int i=0;i<arr.size();i++){
+            occur=((i+1)*(n-i)+1)/2;
+            prod=occur*arr[i];
+            sum=sum+prod;
+        }
+
+        return sum;
+    }
+};
+
+// Reduce Memory;
