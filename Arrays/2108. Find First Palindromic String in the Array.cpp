ca65e14dae6e40603577ff97ca78f4e86929d0bb
Radhaiya <kulkarnir27999@gmail.com>
2022-10-10 21:58:25 +0530

    Optimization 2

--- Arrays/2108. Find First Palindromic String in the Array.cpp
+++ Arrays/2108. Find First Palindromic String in the Array.cpp
@@ -60,3 +60,23 @@ public:
          return "";
     }
 };
+
+
+// More optimization Palindrom in half loop
+
+class Solution {
+public:
+int i;
+bool checkpalindrome(string &str){
+        for(i = 0; i < str.length()/2; i++){
+        if(str[i] != str[str.length()-1-i])return false;
+        }return true;
+}
+
+            string firstPalindrome(vector<string>& words) {
+            for(int i=0;i<words.size();i++){
+                if(checkpalindrome(words[i]))return words[i];
+             }
+                return "";
+}
+};  
